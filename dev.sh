05e612b82216a34402960126e26f62f6ee549df9
Julius Schumacher <juliusschumacher@gmail.com>
2021-05-21 14:24:31 +0200

    added script for easy development

---
+++ dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+export FLASK_ENV=development
+export FLASK_APP=rotary
+
+cd website/rotary/static
+ls ../../..
+sass --sourcemap=none sass/style.scss:css/style.css 2>&1 | tee -a ../../../devlog/sass.log &
+cd ../../
+flask run 2>&1 | tee -a ../devlog/flask.log
