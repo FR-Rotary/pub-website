b37640611cf918132f6f6d607a49de6d5edb8be0
Falk Höppner <falk@hoppner.se>
2021-05-21 15:07:24 +0200

    Remove useless ls

--- dev.sh
+++ dev.sh
@@ -4,7 +4,6 @@ export FLASK_ENV=development
 export FLASK_APP=rotary
 
 cd website/rotary/static
-ls ../../..
 sass --sourcemap=none sass/style.scss:css/style.css 2>&1 | tee -a ../../../devlog/sass.log &
 cd ../../
 flask run 2>&1 | tee -a ../devlog/flask.log
