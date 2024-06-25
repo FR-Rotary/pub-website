c1b5ff7f51e6e851cfc61e60d118189e8773ed79
victor <lannerv@student.chalmers.se>
2024-06-25 15:04:31 +0200

    Improved dev.sh script

--- dev.sh
+++ dev.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Set Flask environment variables
 export FLASK_ENV=development
 export FLASK_APP=rotary
 
+# Change directory to the static assets directory
 cd website/rotary/static
-sass --sourcemap=none sass/style.scss:css/style.css 2>&1 | tee -a ../../../devlog/sass.log &
+
+# Compile Sass to CSS, disable source maps, and log output
+sass --no-source-map sass/style.scss:css/style.css 2>&1 | tee -a ../../../devlog/sass.log &
+
+# Return to the project root directory
 cd ../../
+
+# Run Flask application and log output
 flask run 2>&1 | tee -a ../devlog/flask.log
