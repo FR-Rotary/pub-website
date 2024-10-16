f0dcb257ab7469be85f553bfd74ad35f3b8eeba0
Victor <lannerv@chalmers.se>
2024-10-16 17:34:09 +0200

    dataTables is now installed via NPM instead of CDN

---
+++ website/rotary/static/js/dataTables.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+//import 'datatables.net-dt';
+// import 'datatables.net-dt/css/dataTables.dataTables.css';
+import 'datatables.net-bm';
+import 'datatables.net-bm/css/dataTables.bulma.css';
+
+$(document).ready(function() {
+    $('#beersTable').DataTable({
+        pageLength: 50,
+        lengthMenu: [ [50, 100, 500, -1], [50, 100, 500, "All"] ]
+    });
+    $('#shiftsTable').DataTable({
+        pageLength: 50,
+        lengthMenu: [ [50, 100, -1], [50, 100, "All"] ],
+        order: [[0, 'desc']]
+    });
+});
